3cd74f717e29344e29a9ff63aa3190591fc87447
mickaelmollard-cmd <mickael.mollard@gmail.com>
2026-01-29 12:46:08 +0100

    Create lock.py

---
+++ api/lock.py
@@ -0,0 +1,25 @@
+ from http.server import BaseHTTPRequestHandler
+import json
+import os
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # (optionnel) protection par PIN
+        pin = self.headers.get("X-PIN")
+        if pin != os.environ.get("APP_PIN"):
+            self.send_response(401)
+            self.end_headers()
+            self.wfile.write(b"Invalid PIN")
+            return
+
+        # Réponse OK (on simulera le lock pour l'instant)
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        response = {
+            "action": "lock",
+            "status": "success"
+        }
+
+        self.wfile.write(json.dumps(response).encode())
